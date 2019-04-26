c26fef10ee930416c4aca71e8bd1ebac921e454c
yechao.jia@aiyuangong.com <yechao.jia@aiyuangong.com>
2019-04-26 18:31:48 +0800

    概述看完了

--- src/feature/dogs/dogs.module.ts
+++ src/feature/dogs/dogs.module.ts
@@ -2,6 +2,14 @@ import { Module } from '@nestjs/common';
 import { CatsModule } from './../cats/cats.module';
 import { DogsController } from './dogs.controller';
 
+class OptionsProvider {
+    
+}
+const connectionFactory = {
+    provider: 'CONNECTION',
+    userFactory: async (optionsProvider: OptionsProvider) => {}
+}
+
 @Module({
     imports: [CatsModule],
     controllers: [DogsController]
